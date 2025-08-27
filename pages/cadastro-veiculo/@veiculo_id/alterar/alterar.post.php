c48298af2143f43d42eb612add6636f0a663d53c
Marcos Gonçalves <marcos.ggoncalves.pr@gmail.com>
2025-08-27 01:10:57 -0300

    fix: correção na edição do veiculo

--- pages/cadastro-veiculo/@veiculo_id/alterar/alterar.post.php
+++ pages/cadastro-veiculo/@veiculo_id/alterar/alterar.post.php
@@ -6,18 +6,21 @@
         $descricao = filter_input(INPUT_POST, 'descricao_veiculo', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $lugares   = filter_input(INPUT_POST, 'lugares', FILTER_VALIDATE_INT);
         $categoria = filter_input(INPUT_POST, 'id_categoria', FILTER_VALIDATE_INT);
+        $vaga = filter_input(INPUT_POST, 'id_vaga', FILTER_VALIDATE_INT);
+
         $idVeiculo = $_PAR['veiculo_id'];
 
         $body = [
             'placa_veiculo'     =>  $placa,  
             'descricao_veiculo' => $descricao,
             'lugares'           => $lugares,
-            'id_categoria'      => $categoria
+            'id_categoria'      => $categoria,
+            'id_vaga' => $vaga
         ];
 
         $service->update('veiculos', $body, ['id_veiculo' =>  $idVeiculo]);
 
-        makeCb(1, 'Cadastrado alterado com sucesso!');
+        makeCb(1, 'Cadastro alterado com sucesso!');
         
         header("Location: /");
     } catch (\Throwable $th) {
